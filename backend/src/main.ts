f617e4be248c81b82793c2ee0f9e909039cfa192
fawaz <fawaz.kourdoughli@gmail.com>
2025-09-09 18:03:34 +0300

    .

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: 'https://davinc-task-8xmbo7017-fawazzos-projects.vercel.app',
+    origin: 'https://davinc-task.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
